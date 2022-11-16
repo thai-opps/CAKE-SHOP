3aa0ff0335d12a61bafcb972d3c54cf891ee1cb8
thai-opps <tchau2214@gmail.com>
2022-11-16 13:06:36 +0700

    add new change file

--- assets/js/home_page.js
+++ assets/js/home_page.js
@@ -8,3 +8,7 @@ function showSearch(){
     var x = document.getElementById("formID");
     x.classList.toggle('show__search-form')
 }
+function showMenu(){
+    var x = document.getElementById("menuBTN");
+    x.classList.toggle('show__menu')
+}
